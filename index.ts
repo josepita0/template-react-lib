ed629c322c53a6f5725157d2f0419c81497e780c
José Acurero Pita <91859867+josepita0@users.noreply.github.com>
2024-08-21 11:23:46 -0400

    Initial commit

---
+++ index.ts
@@ -0,0 +1,5 @@
+"use client";
+
+export * from "./src/components";
+export * from "./src/lib/utils";
+export * from "./src/lib/hooks";
---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+"use client";
+
+export * from "./form-builder/form-builder";
+export * from "./form-builder/form-builer.interface";
+export * from "./form-builder/form-field";
+export * from "./ui/button";
+export * from "./ui/form";
+export * from "./ui/label";
